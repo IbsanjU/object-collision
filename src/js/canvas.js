0f151907feadd370fe4189973277eebc4452b960
IbsanjU <bharath2668@gmail.com>
2022-06-15 20:14:33 +0530

    bounce off edges

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -56,6 +56,12 @@ class Circle {
 				console.log('collided')
 			}
 		}
+		if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
+			this.velocity.x = -this.velocity.x
+		}
+		if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height) {
+			this.velocity.y = -this.velocity.y
+		}
 		this.x += this.velocity.x
 		this.y += this.velocity.y
 	}
